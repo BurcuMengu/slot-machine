8d6431abc13921b986496ae3bb315718bf4f1d72
BurcuMengu <burcumengu@gmail.com>
2026-02-19 17:50:49 +0300

    fix numberOfLines validation condition

--- main.js
+++ main.js
@@ -17,7 +17,7 @@ const getNumberOfLines = () => {
         const lines = prompt("Enter the number of lines to bet on (1-3): ");
         const numberOfLines = parseFloat(lines);
 
-        if(isNaN(numberOfLines) || numberOfLines <= 0 || numberOfLines < 3) {
+        if(isNaN(numberOfLines) || numberOfLines <= 0 || numberOfLines > 3) {
             console.log("Invalid number of lines, try again.");
         } else {
             return numberOfLines;
